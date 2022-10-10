1badf3bdc9514a9d93d651aee415fc48f3d8ea41
ajayttrekkie <ajay@techarttrekkies.com>
2022-10-10 11:23:21 +0545

    wqdas

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -1,51 +1,44 @@
 export const serverLocation = "http://127.0.0.1:8001/api";
-  
 
 export const fileUrl = "http://127.0.0.1:8001/storage";
 
-export const pagination=10;
+export const pagination = 10;
 
 export const URL = {
-    registerUser:`${serverLocation}/free/register-user`,
-    login:`${serverLocation}/free/login`,
-    logout:`${serverLocation}/auth/logout`,
+  registerUser: `${serverLocation}/free/register-user`,
+  login: `${serverLocation}/free/login`,
+  logout: `${serverLocation}/auth/logout`,
 
+  // modules
+  modules: `${serverLocation}/free/modules`,
 
+  // categories
+  categories: `${serverLocation}/auth/categories`,
+  storeCategory: `${serverLocation}/auth/categories/store`,
+  deleteCategory: `${serverLocation}/auth/categories/delete`,
+  searchCategory: `${serverLocation}/auth/categories/search`,
+  getAllCategories: `${serverLocation}/auth/categories/get-all-categories`,
 
+  // roles
+  roles: `${serverLocation}/auth/roles`,
+  storeRole: `${serverLocation}/auth/roles/store`,
+  editRolePermissions: `${serverLocation}/auth/roles/edit-role-permissions`,
+  deleteRole: `${serverLocation}/auth/roles/delete`,
+  searchRole: `${serverLocation}/auth/roles/search`,
 
-    // modules
-    modules:`${serverLocation}/free/modules`,
+  // permissions
+  permissions: `${serverLocation}/auth/permissions`,
+  modulePermissions: `${serverLocation}/auth/permissions/module-permissions`,
+  moduleRolePermissions: `${serverLocation}/auth/permissions/module-role-permission`,
 
-    // categories
-    categories:`${serverLocation}/auth/categories`,
-    storeCategory:`${serverLocation}/auth/categories/store`,
-    deleteCategory:`${serverLocation}/auth/categories/delete`,
-    searchCategory:`${serverLocation}/auth/categories/search`,
-    getAllCategories:`${serverLocation}/auth/categories/get-all-categories`,
-
-    // roles
-    roles:`${serverLocation}/auth/roles`,
-    storeRole:`${serverLocation}/auth/roles/store`,
-    editRolePermissions:`${serverLocation}/auth/roles/edit-role-permissions`,
-    deleteRole:`${serverLocation}/auth/roles/delete`,
-    searchRole:`${serverLocation}/auth/roles/search`,
-
-
-    // permissions
-    permissions:`${serverLocation}/auth/permissions`,
-    modulePermissions:`${serverLocation}/auth/permissions/module-permissions`,
-    moduleRolePermissions:`${serverLocation}/auth/permissions/module-role-permission`,
-
-    // nominal-accounts
-    insertNominalAccounts:`${serverLocation}/auth/nominal-accounts/store`,
-    nominalAccounts:`${serverLocation}/auth/nominal-accounts`,
-   
+  // nominal-accounts
+  insertNominalAccounts: `${serverLocation}/auth/nominal-accounts/store`,
+  nominalAccounts: `${serverLocation}/auth/nominal-accounts`,
 };
 
-
 export const headers = {
-    Authorization: `Bearer ${localStorage.getItem("token")} `,
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    // "Client-Ip": localStorage.getItem("my_ip"),
-  };
+  Authorization: `Bearer ${localStorage.getItem("token")} `,
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  // "Client-Ip": localStorage.getItem("my_ip"),
+};
